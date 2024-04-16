9c1933887ac6d1ef24c0793e4dfff569597d4a35
Walther Medina <auxiliarTI@clubinter.org.pe>
2024-04-16 16:35:27 -0500

    Successful Db connection

--- ClubInterV2/DTOs/Account/RegisterDto.cs
+++ ClubInterV2/DTOs/Account/RegisterDto.cs
@@ -5,10 +5,10 @@ namespace ClubInterV2.DTOs.Account
     public class RegisterDto
     {
         [Required]
-        [RegularExpression("^\\w+@[a-zA-Z_]+?\\.[a-zA-Z]{2,3}$", ErrorMessage = "Invalid email address")
+        [RegularExpression("^\\w+@[a-zA-Z_]+?\\.[a-zA-Z]{2,3}$", ErrorMessage = "Invalid email address")]
         public string Usuario { get; set; }
         [Required]
-        [RegularExpression("^\\w+@[a-zA-Z_]+?\\.[a-zA-Z]{2,3}$", ErrorMessage = "Invalid email address")
+        [RegularExpression("^\\w+@[a-zA-Z_]+?\\.[a-zA-Z]{2,3}$", ErrorMessage = "Invalid email address")]
         public string Email { get; set; }
         [Required]
         [StringLength(15, MinimumLength = 6, ErrorMessage = "Password must be at least {2}, and maximun {1} characters")]
